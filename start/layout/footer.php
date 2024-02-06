d324f3497ac19f6e0f451d23a8f44cb340e6b07c
Rochelle Mullen <b00156311@mytudublin.ie>
2024-02-06 17:37:28 +0000

    rm

---
+++ start/layout/footer.php
@@ -0,0 +1,12 @@
+
+<footer>
+    <p>&copy; AirPupNMeow.com</p>
+</footer>
+</div>
+<!-- /container -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
